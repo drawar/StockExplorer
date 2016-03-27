d8019aeb0cbef2311c0b6e5701a34d8f95426727
drawar <van_igo94@yahoo.com>
2016-03-27 23:48:21 +0800

    Shiny application: Stock Explorer

--- server.R
+++ server.R
@@ -1,37 +1,182 @@
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
-library(ggplot2)
+library(quantmod)
+library(xtable)
+library(PerformanceAnalytics)
+library(rugarch)
 
-function(input, output) {
+#Function to capitalize the first letter of a string
+proper <- function(x) paste0(toupper(substr(x, 1, 1)), tolower(substring(x, 2)))
+
+# Define server logic for random distribution application
+shinyServer(function(input, output) {
   
-  dataset <- reactive({
-    diamonds[sample(nrow(diamonds), input$sampleSize),]
+  # acquiring data
+  dataInput <- reactive({
+    if (input$get == 0)
+      return(NULL)
+    
+    return(isolate({
+      getSymbols(input$symb,src="yahoo", auto.assign = FALSE)
+    }))
   })
   
-  output$plot <- renderPlot({
+  datesInput <- reactive({
+    if (input$get == 0)
+      return(NULL)
     
-    p <- ggplot(dataset(), aes_string(x=input$x, y=input$y)) + geom_point()
-    
-    if (input$color != 'None')
-      p <- p + aes_string(color=input$color)
-    
-    facets <- paste(input$facet_row, '~', input$facet_col)
-    if (facets != '. ~ .')
-      p <- p + facet_grid(facets)
-    
-    if (input$jitter)
-      p <- p + geom_jitter()
-    if (input$smooth)
-      p <- p + geom_smooth()
-    
-    print(p)
-    
-  }, height=700)
+    return(isolate({
+      paste0(input$dates[1], "::",  input$dates[2])
+    }))
+  })
   
-}
+  
+  # tab based controls
+  output$newBox <- renderUI({
+    switch(input$tab,
+           "Prices" = chartControls,
+           "Returns" = returnControls,
+           "VaR" = varControls
+    )
+  })
+  
+  # Charts tab
+  chartControls <- div(
+    wellPanel(
+      selectInput("chart_type",
+                  label = "Chart type",
+                  choices = c("Candlestick" = "candlesticks", 
+                              "Matchstick" = "matchsticks",
+                              "Bar" = "bars",
+                              "Line" = "line"),
+                  selected = "Line"
+      ),
+      checkboxInput(inputId = "log_y", label = "log y axis", 
+                    value = FALSE)
+    ),
+    
+    wellPanel(
+      p(strong("Technical Analysis")),
+      checkboxInput("ta_vol", label = "Volume", value = FALSE),
+      checkboxInput("ta_sma", label = "Simple Moving Average", 
+                    value = FALSE),
+      checkboxInput("ta_ema", label = "Exponential Moving Average", 
+                    value = FALSE),
+      checkboxInput("ta_wma", label = "Weighted Moving Average", 
+                    value = FALSE),
+      checkboxInput("ta_bb", label = "Bolinger Bands", 
+                    value = FALSE),
+      checkboxInput("ta_momentum", label = "Momentum", 
+                    value = FALSE),
+      
+      br(),
+      
+      actionButton("chart_act", "Add Technical Analysis")
+    )
+  )
+  
+  TAInput <- reactive({
+    if (input$chart_act == 0)
+      return("NULL")
+    
+    tas <- isolate({c(input$ta_vol, input$ta_sma, input$ta_ema, 
+                      input$ta_wma,input$ta_bb, input$ta_momentum)})
+    funcs <- c(addVo(), addSMA(), addEMA(), addWMA(), 
+               addBBands(), addMomentum())
+    
+    if (any(tas)) funcs[tas]
+    else "NULL"
+  })
+  
+  output$chart <- renderPlot({
+    chartSeries(dataInput(),
+                name = input$symb,
+                type = input$chart_type,
+                subset = datesInput(),
+                log.scale = input$log_y,
+                theme = "white",
+                TA = TAInput())
+  })
+  
+  
+  # Returns tab
+  returnControls <- div(
+    wellPanel(
+      selectInput("period",
+                  label = "Periodicity",
+                  choices = c("Daily" = "daily", 
+                              "Weekly" = "weekly",
+                              "Monthly" = "monthly",
+                              "Quarterly" = "quarterly",
+                              "Yearly" = "yearly"),
+                  selected = "Daily"
+      ),
+      selectInput("type",
+                  label = "Arithmetic/Log Return",
+                  choices = c("Arithmetic" = "arithmetic", 
+                              "Log" = "log"),
+                  selected = "Log"
+      )
+    ))
+  returns <- reactive({ 
+    if (input$get == 0)
+      return(NULL)
+    
+    periodReturn(dataInput(), period = input$period, type = input$type)
+  })
+
+  
+  output$chart2 <- renderPlot({
+    chartSeries(returns(),
+                name = paste(paste(paste(input$symb,"'s",sep=""),proper(input$period),sep=" "),"Returns", sep=" "),
+                type = "line",
+                subset = datesInput(),
+                theme = "white",
+                TA = NULL)
+  })
+  output$summary <- renderTable({
+    table.Stats(returns())
+  },caption = "Summary Statistics",
+  caption.placement = getOption("xtable.caption.placement", "top"), 
+  caption.width = getOption("xtable.caption.width", NULL))
+  # VaR tab
+  varControls <- div(
+    wellPanel(
+      selectInput("var_act",
+                  label = "Confidence Level",
+                  choices = c("90%" = 0.1, 
+                              "95%" = 0.05,
+                              "99%" = 0.01)
+      )
+    )
+  )
+#   confInput <- reactive({
+#     if (input$var_act == 0)
+#       return("NULL")
+#     
+#     tas <- isolate({c(input$ta_90, input$ta_95, input$ta_99)})
+#     funcs <- c(0.1,0.05,0.01)
+#     
+#     if (any(tas)) funcs[tas]
+#     else "NULL"
+#   })
+  
+  var <- reactive({
+    spec <- ugarchspec(mean.model = list(armaOrder = c(1,0)), 
+                       variance.model = list(garchOrder = c(1,1), 
+                                             model = "gjrGARCH", 
+                                             variance.targeting=FALSE), 
+                       distribution.model = "sstd")
+    fit <- ugarchfit(spec, returns())
+    quantile(fit, input$var_act)
+  })
+  output$chart3 <- renderPlot({
+    chartSeries(var(),
+                name = paste(paste(paste(input$symb,"'s",sep=""), proper(input$period),sep=" "),"VaR", sep=" "),
+                type = "line",
+                subset = datesInput(),
+                theme = "white",
+                TA = NULL)
+  })
+  output$text3 <- renderText({paste0(input$symb, " 3: ", input$tab)})
+  
+})
--- ui.R
+++ ui.R
@@ -1,36 +1,61 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
-library(ggplot2)
 
-dataset <- diamonds
-
-fluidPage(
+# Define UI for random distribution application 
+shinyUI(pageWithSidebar(
   
-  titlePanel("Diamonds Explorer"),
+  headerPanel("Stock Explorer"),
   
   sidebarPanel(
     
-    sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset),
-                value=min(1000, nrow(dataset)), step=500, round=0),
+    helpText("Select a stock by typing in the box below. All data are sourced from Yahoo Finance."),
     
-    selectInput('x', 'X', names(dataset)),
-    selectInput('y', 'Y', names(dataset), names(dataset)[[2]]),
-    selectInput('color', 'Color', c('None', names(dataset))),
+    textInput("symb", "Symbol", placeholder = 'Type a ticker symbol, e.g. GOOG'),
+#     selectizeInput("symb", label = "Symbol", choices = NULL, options = list(
+#       placeholder = 'Type a ticker symbol, e.g. GOOG', maxOptions = 5)
+#     ),
+    dateRangeInput("dates", 
+                   "Dates",
+                   start = "2013-01-01", end = Sys.Date()),
     
-    checkboxInput('jitter', 'Jitter'),
-    checkboxInput('smooth', 'Smooth'),
+    actionButton("get", "Get Stock"),
     
-    selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
-    selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
-  ),
+    br(),
+    br(),
+    
+    uiOutput("newBox")
+    
+    ),
   
+  # Show a tabset that includes a plot, summary, and table view
+  # of the generated distribution
   mainPanel(
-    plotOutput('plot')
+    tabsetPanel(
+      tabPanel("Prices", plotOutput("chart")), 
+      tabPanel("Returns", plotOutput("chart2"), tableOutput("summary")), 
+      tabPanel("VaR", plotOutput("chart3")),
+      tabPanel("How To",
+               HTML("<p> This Shiny application can be used to collect historical stock prices and perform relevant technical analyses. The application depends on the following packages to function properly: quantmod, xtable, PerformaceAnalytics, and rugarch. Below is a description of how this application works, organized by tabs.</p>
+                
+                    <b>Prices</b>
+                    <p>This tab provides basic tools to visualize the price movement of a particular stock over time.</p>
+                    <li>Symbol:</li> Type in the ticker symbol of the stock you want to examine e.g. MSFT(Microsoft). 
+                    <li>Dates:</li> Select the starting and ending dates of the period you want to examine. Ending date is set as system date by default.
+                    <p>Hit 'Get Stock' to apply your selection.</p>
+                    <li>Chart type:</li> Different chart types (line, candlestick, matchstick, and bar) are available from the drop-down menu. 
+                    <li>Log y axis:</li> Select this option if you want the y-axis to be log scaled. When the increment on the y-axis is small it may be helpful to check this box.
+                    <li>Technical analysis:</li> Add or overlay additional plots of technical indicators that are useful for making trading decisions. 
+                    <p>When you are satisfied with your selection, hit 'Add Technical Analysis' to see the result.</p>
+                    <b>Returns</b>
+                    <p>This tab allows you to convert prices to returns of your chosen method and periodicity. A table of summary statistics is also displayed.</p>
+                    <li>Periodicity:</li> Determine the time period of your returns such as daily/weekly/etc.
+                    <li>Arithmetic/Log return:</li> Whether the arithmetic (discrete) or log (continuous) transformation is used.
+                    <p></p>
+                    <b>VaR</b>
+                    <p>This tab computes the Value-at-Risk (VaR) at a given confidence level using parametric method by fitting an ARMA(1,0)-GJR-GARCH(1,1) model with skewed Student-t innovations to the returns that you have calculated.</p>
+                    <li>Confidence level:</li> Select a confidence level. Available options are 90%, 95%, and 99%.
+                    ")),
+
+      id = "tab"
+    )
   )
-)
+))
